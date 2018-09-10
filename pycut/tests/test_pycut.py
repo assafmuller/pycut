0cd3c41c5ac505c7a5c79d7ba942d32989ae702f
Assaf Muller <amuller@redhat.com>
2018-09-10 17:05:00 -0400

    Add more tests

--- pycut/tests/test_pycut.py
+++ pycut/tests/test_pycut.py
@@ -12,4 +12,17 @@ class PycutTestCase(tests_base.BaseTestCase):
         self.assertEqual('00000', pycut.cut(value, '3', delimiter))
 
     def test_field_range(self):
-        pass
+        value = 'some_file.json.00000'
+        delimiter = '.'
+
+        self.assertEqual('some_file.json', pycut.cut(value, '1-2', delimiter))
+        self.assertEqual('json.00000', pycut.cut(value, '2-3', delimiter))
+        self.assertEqual('some_file.json.00000', pycut.cut(value, '1-3', delimiter))
+
+    def test_open_field_range(self):
+        value = 'some_file.json.00000'
+        delimiter = '.'
+
+        self.assertEqual('some_file.json', pycut.cut(value, '-2', delimiter))
+        self.assertEqual('some_file.json.00000', pycut.cut(value, '1-', delimiter))
+        self.assertEqual('json.00000', pycut.cut(value, '2-', delimiter))
